b0206088f91d1e2a846b3777783f28d5b491d376
chaden ben ammar <59190965+ChadenBA@users.noreply.github.com>
2025-11-19 21:18:03 +0100

    Update TryHackMeData.ts

--- src/data/TryHackMeData.ts
+++ src/data/TryHackMeData.ts
@@ -1,11 +1,11 @@
 export const tryHackMeStats = {
     profileBadge: "https://tryhackme.com/api/v2/badges/public-profile?userPublicId=5104093", 
-    totalRooms: 377,
+    totalRooms: 430,
     globalRank: "",
-    rankPercent: "Top 1% (1746) 0xF [TITAN]",
-    totalBadges: 57,
-    totalPaths: 7,
+    rankPercent: "Top 1% (1437) 0xF [TITAN]",
+    totalBadges: 65,
+    totalPaths: 8,
     points: 1746,
-    streak: 52,
+    streak: 102,
   };
   
